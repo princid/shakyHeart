fa4580f3207c5e70acde78d83af7ed688d076ea6
Prince Kumar Singh <90444477+princid@users.noreply.github.com>
2023-09-04 10:06:40 +0530

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+//row me kro pehle
+
+// function chamaktaSitara(){
+//     let inputNum = document.getElementById('inputNum').value;
+//     if(inputNum>=1 && inputNum<=5){
+//         for(let i = inputNum; i <= 5; i++){
+//             let sitara = document.getElementById(i);
+//             sitara.classList.add('active', 'fa-beat-fade');
+//         }
+//     }
+//     else{
+//         // alert("1 se 5 ke alawa Nahi Chamkega re burbak!!");
+//         alert("1 se 5 ke alawa Nahi Chamkunga!!");
+//         console.log("OKAY BRO :)");
+//     }
+// }
+
+
+//column me kro ab
+
+function chamaktaSitara() {
+  let inputNum = document.getElementById("inputNum").value;
+  if (inputNum >= 1 && inputNum <= 5) {
+    for (let i = 1; i <= 5; i++) {
+      for (let j = 1; j <= inputNum; j++) {
+        let sitara = document.getElementById(j);
+        console.log(sitara);
+        sitara.classList.add("active", "fa-beat-fade");
+      }
+    }
+  } 
+  else {
+    alert("1 se 5 ke alawa Nahi Chamkunga!!");
+    console.log("OKAY BRO :)");
+  }
+}
